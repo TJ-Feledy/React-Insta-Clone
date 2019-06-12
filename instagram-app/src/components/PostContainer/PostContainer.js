fd2b0b7a1e9d30b6e1794544d7962267c76f1d8e
teej387 <teej387@yahoo.com>
2019-06-12 18:55:48 -0500

    Ran .map on props in PostContainer, returned CommentSection with obj.comments

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import './PostContainer.css'
+import React from 'react';
+import '../PostContainer/PostContainer.css';
+import CommentSection from '../CommentSection/CommentSection';
 
 function PostContainer(props) {
   return (
     <div className='postContainer'>
-      {console.log(props)}
+      {console.log(props.object)}
+      {props.map((obj, index) => {
+        return <CommentSection comments={obj.comments} key={index} />
+      })}
     </div>
   )
 }
