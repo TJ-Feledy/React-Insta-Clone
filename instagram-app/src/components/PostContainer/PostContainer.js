af3a1c78813574cb955dbbcec6d348eebe706b5b
teej387 <teej387@yahoo.com>
2019-06-12 21:25:02 -0500

    Ran type checking on props coming through PostContainer Component.

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import '../PostContainer/PostContainer.css';
+import PropTypes from 'prop-types';
 import CommentSection from '../CommentSection/CommentSection';
+
+import '../PostContainer/PostContainer.css';
 
 function PostContainer(props) {
   return (
     <div className='postContainer'>
-      {/* {console.log(props.object.comments)} */}
+      {console.log(props.object.comments)}
       <CommentSection comments={props.object.comments} />
     </div>
   )
+};
+
+PostContainer.propTypes = {
+  object: PropTypes.shape({
+    id: PropTypes.string,
+    username: PropTypes.string,
+    thumbnailUrl: PropTypes.string,
+    comments: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number,
+      username: PropTypes.string,
+      text: PropTypes.string
+    })),
+    imageUrl: PropTypes.string,
+    likes: PropTypes.number,
+    timestamp: PropTypes.string,
+
+  })
 }
 
 export default PostContainer
